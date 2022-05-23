5775d9d3c657697da616e7e9ce0f8d67c2d644d2
Jian Kim <jian7061@gmail.com>
2022-05-23 12:19:09 +0900

    adding the marketplace to the app

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ const CONTRACT_NAME = "mycontract.testjianaccount.testnet";
 
 function environment(env) {
   switch (env) {
-    case "mainnet":
+    case "mainnet": // line 5
       return {
         networkId: "mainnet",
         nodeUrl: "https://rpc.mainnet.near.org",
@@ -11,7 +11,7 @@ function environment(env) {
         helperUrl: "https://helper.mainnet.near.org",
         explorerUrl: "https://explorer.mainnet.near.org",
       };
-    case "testnet":
+    case "testnet": // line 14
       return {
         networkId: "testnet",
         nodeUrl: "https://rpc.testnet.near.org",
--- src/utils/near.js
+++ src/utils/near.js
@@ -8,22 +8,18 @@ export async function initializeContract() {
   //near object  to interact with the NEAR network. holds a keyStores object that stores the wallet information in brower's local storage.
   const near = await connect(
     Object.assign(
-      {
-        deps: {
-          keyStores: new keyStores.BrowserLocalStorageKeyStore(),
-        },
-      },
+      { deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } },
       nearEnv
     )
   );
   //WalletConnection object to interact with the wallet.(To sign in, sign out, get accountId, get account balance).
-  window.WalletConnection = new WalletConnection(near);
-  window.accountId = window.WalletConnection.getAccountId();
+  window.walletConnection = new WalletConnection(near);
+  window.accountId = window.walletConnection.getAccountId();
   //Contract object to interact with the smart contract.
   //pass the account, the name of the smart contract, and the methods we want to use.
   //When developing with NEAR, not need ABI
   window.contract = new Contract(
-    window.WalletConnection.account(),
+    window.walletConnection.account(),
     nearEnv.contractName,
     {
       viewMethods: ["getProduct", "getProducts"],
@@ -34,20 +30,20 @@ export async function initializeContract() {
 
 export async function accountBalance() {
   return formatNearAmount(
-    (await window.WalletConnection.account().getAccountBalance()).total,
+    (await window.walletConnection.account().getAccountBalance()).total,
     2
   );
 }
 
 export async function getAccountId() {
-  return window.WalletConnection.getAccountId();
+  return window.walletConnection.getAccountId();
 }
 
 export function login() {
-  window.WalletConnection.requestSignIn(nearEnv.contractName);
+  window.walletConnection.requestSignIn(nearEnv.contractName);
 }
 
 export function logout() {
-  window.WalletConnection.signOut();
+  window.walletConnection.signOut();
   window.location.reload();
 }
